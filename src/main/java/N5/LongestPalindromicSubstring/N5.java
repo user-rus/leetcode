6bc06605868a5d2b0d56f72a99d596ef0fe1e8ed
user-rus <sqr@criptext.com>
2024-03-20 11:52:54 +0300

    commit

--- src/main/java/N5/LongestPalindromicSubstring/N5.java
+++ src/main/java/N5/LongestPalindromicSubstring/N5.java
@@ -1,17 +1,14 @@
 package N5.LongestPalindromicSubstring;
 
-public class N5 {
-    public static boolean isPalindrome(String str) {
-        StringBuilder reversed = new StringBuilder(str).reverse();
-        return str.equals(reversed.toString());
-    }
-    public String longestPalindrome(String s) {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
-        return "sdfsdf";
-    }
+public class N5 {
+
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("ab"));
+
 
     }
 }
